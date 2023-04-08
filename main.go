4e02b2fa0f983d57f6589755a1b1e142b9506aef
TheBigSteph <nzali.stephane86@gmail.com>
2023-04-08 17:05:17 +0100

    project settings

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/TheBigSteph/fullstack-test/configs"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main () {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
+	})
+
+	configs.ConnectDB()
+
+	app.Listen(":8080")
+}
